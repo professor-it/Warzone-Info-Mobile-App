d6c698c3ca328499bfc8821fcb60ccac28052992
Dima Nikolaenko <dima.nikolaenko.ua@gmail.com>
2021-08-06 21:50:28 +0300

    Change TextRich

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import {ScrollView, View, Text, StyleSheet, Image} from 'react-native'
+import {ScrollView, View, Text, StyleSheet, Image, useWindowDimensions} from 'react-native'
 import {THEME} from '../theme'
+import * as showdown from 'showdown'
+import HTML, {defaultSystemFonts} from 'react-native-render-html'
 
 export const PostScreen = ({navigation}) => {
 	const post = navigation.getParam('post')
 
+	const converter = new showdown.Converter({
+		tables: true,
+		simplifiedAutoLink: false,
+		strikethrough: true,
+		tasklists: true,
+		noHeaderId: true,
+	})
+	const htmlPost = converter.makeHtml(post.text)
+	const contentWidth = useWindowDimensions().width;
+	const fonts = [...defaultSystemFonts, 'open-regular', 'open-bold']
 
 	return (
 		<ScrollView style={styles.wrapperMain}>
@@ -14,7 +26,7 @@ export const PostScreen = ({navigation}) => {
 			<View style={styles.wrapper}>
 				<Text style={styles.title}>{post.title}</Text>
 				<Text style={styles.desc}>{post.desc}</Text>
-				<Text style={styles.text}>{post.text}</Text>
+				<HTML source={{html: htmlPost}} contentWidth={contentWidth} systemFonts={fonts} tagsStyles={stylesText} enableUserAgentStyles={true}/>
 			</View>
 		</ScrollView>
 	)
@@ -62,3 +74,45 @@ const styles = StyleSheet.create({
 		height: '100%',
 	}
 })
+
+const stylesText = {
+	body: {
+		color: '#ffffff',
+		fontFamily: 'open-regular'
+	},
+	p: {
+		marginTop: 0,
+		marginVertical: '0.4em'
+	},
+	h3: {
+		color: '#fff',
+		fontFamily: 'open-bold',
+		fontSize: '1.17em',
+		marginVertical: '1em'
+	},
+	h2: {
+		color: '#fff',
+		fontFamily: 'open-bold',
+		fontSize: '1.5em',
+		marginVertical: '0.83em'
+	},
+	h1: {
+		color: '#fff',
+		fontFamily: 'open-bold',
+		fontSize: '2em',
+		marginVertical: '0.67em'
+	},
+	a: {
+		color: '#fff',
+	},
+	blockquote: {
+		color: '#fff',
+		marginVertical: '1.12em',
+		backgroundColor: 'rgba(0,0,0,0.8)',
+		fontStyle: 'italic',
+	},
+	table: {
+		color: '#fff',
+		fontFamily: 'open-regular'
+	}
+}
